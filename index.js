b2bb379bf2a50d8b1d508abce936236676f8bdd8
freebear1254 <freebear1254@gmail.com>
2021-08-31 17:49:54 +0900

    function userSave update

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
-
 const express = require('express')
 const app = express()
 const port = 3000
 
+const config = require('./config/key');
+
+const bodyParser = require('body-parser');
+const {User} = require('./models/User');
+
+
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+//application/json
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI,{
+    useNewUrlParser : true, useUnifiedTopology: true
+}).then(()=>console.log("Mongo Connected")).catch(err => console.log(err));
+
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World! Study Node.js')
 })
+
+app.post('/register',(req,res)=>{
+    //회원가입 정보를 받아 DB에 저장
+
+    const user = new User(req.body);
+    user.save((err,userInfo)=>{
+        if(err) return res.json({success:false,err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+
+
+})
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
-
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://node:sks1254@testcluster.chx6t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
-    useNewUrlParser : true, useUnifiedTopology: true
-}).then(()=>console.log("Mongo Connected")).catch(err => console.log(err));
